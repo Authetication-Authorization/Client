093df2b2f3725a35c74b4a056cc597f00e712990
chinthaka senarathna <chironprabath@gmail.com>
2025-04-22 22:12:24 +0530

    done the validation ???

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './registration.component.html',
   styles: ``
 })
@@ -14,15 +15,56 @@ export class RegistrationComponent implements OnInit {
   constructor(public formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.form = this.formBuilder.group({
-      fullName: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
-    })
+    this.form = this.formBuilder.group(
+    {
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.passwordValidator]],
+      confirmPassword: ['', [Validators.required]],
+    },
+    {
+      validators: this.matchPasswords
+    });
+  }
+
+  matchPasswords(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+  
+    return password === confirmPassword ? null : { passwordsDontMatch: true };
+  }
+  
+
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (!value) return null;
+
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasLowerCase = /[a-z]/.test(value);
+    const hasNumber = /[0-9]/.test(value);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+    const isValidLength = value.length >= 8;
+
+    const passwordValid = hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && isValidLength;
+
+    return !passwordValid ? {
+      passwordStrength: {
+        hasUpperCase,
+        hasLowerCase,
+        hasNumber,
+        hasSpecialChar,
+        isValidLength
+      }
+    } : null;
   }
 
   onSubmit() {
-    console.log(this.form.value);
+    if(this.form.valid) {
+      console.log(this.form.value);
+      this.form.reset();
+    }
+    else {
+      console.log('Form is invalid');
+    }
   }
 }
